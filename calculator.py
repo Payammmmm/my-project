d6f4bfdcb613298918e4841d1b94e56a32a0fca9
Payammmmm <122191315+Payammmmm@users.noreply.github.com>
2025-05-21 03:34:34 -0700

    Add files via upload

---
+++ calculator.py
@@ -0,0 +1,23 @@
+print("if you write -1 exit program")
+num2=0
+num1=0
+while num1 != -1:
+      
+      num1=int(input('enter number one: '))
+      if num1== -1:
+           print('exiting program ...')
+           break
+      oper=input('enter your operator: ')
+      num2=int(input('enter number two: '))
+      if oper=='+':
+         jam=num1+num2
+         print(jam)
+      elif oper=='-':
+           tafrigh=num1-num2
+           print(tafrigh)
+      elif oper=='*':
+           zarb=num1*num2
+           print(zarb)
+      elif oper=='/':
+           taghsim=num1/num2
+           print(taghsim)
